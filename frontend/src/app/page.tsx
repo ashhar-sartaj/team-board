a1ef6245e2380f87d58aabe179d4f97d280bd22d
Ashhar Sartaj <ashharsartaj04@gmail.com>
2026-03-15 23:42:58 +0530

    frontend: login button added to navigate to '/login' from homepage

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,19 @@
+"use client"
 import Image from "next/image";
-
+import { useRouter } from "next/navigation"
 export default function Home() {
+  const router = useRouter();
+  const handleClick = () => {
+    router.push("/login")
+  }
   return (
-    <main className="flex min-h-screen items-center justify-center">
+    <main className="flex flex-col min-h-screen items-center justify-center">
       <h1 className="text-3xl font-bold">
         Team Collaboration Board
       </h1>
+      <button className="bg-white border-2 border-black text-black py-2 px-6 m-2 cursor-pointer" onClick={handleClick} type="button">
+        Login
+      </button>
     </main>
   );
 }
