afb36d589c3b62f2b40aff7dd7779fd26cb17e63
Ashhar Sartaj <ashharsartaj04@gmail.com>
2026-03-15 22:43:02 +0530

    backend: start and dev script should only be using single tool that is tsx

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,7 +6,7 @@ import dns from 'node:dns';
 dns.setServers(['8.8.8.8', '1.1.1.1']);
 const httpServer = http.createServer(app);
 dotenv.config()
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 dbConnection();
 httpServer.listen(PORT, () => {
     console.log('Server is running on port ', PORT);
