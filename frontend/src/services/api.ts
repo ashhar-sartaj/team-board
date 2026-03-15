78ef2e009661118505642c2f6a56518f62a54374
Ashhar Sartaj <ashharsartaj04@gmail.com>
2026-03-15 23:04:39 +0530

    frontend: baseURL changed to process.env.NEXT_PUBLIC_API_URL

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 const api = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL || 'http://localhost:5000/api'
+    baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api'
 })
 export default api;
